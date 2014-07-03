4d2e1ef972ba7eb050b50be172e328763e04b2ee
Daniel Cortes <daniel.cortes@punchkickinteractive.com>
2014-07-03 17:08:33 -0500

    Added alert to Designer News.

--- contentscript.js
+++ contentscript.js
@@ -6,8 +6,47 @@ if (host === 'news.ycombinator.com') {
 	$('.comhead:contains("medium.com")').parent().parent().next().css('display', 'none');
 	//remove title row
 	$('.comhead:contains("medium.com")').parent().parent().css('display', 'none');
-} else if (host === 'news.layervault.com') {
-	$('span:contains("medium.com")').parent().parent().css('display', 'none');
-} else {
+}
+else if (host === 'news.layervault.com') {
+	//count removed items
+	var howMany = $('span:contains("medium.com")').length;
+
+	if (howMany > 0) {
+		//remove items
+		$('span:contains("medium.com")').parent().parent().css('display', 'none');
+
+		//say how many were removed
+		//singular or plural?!
+		if (howMany < 2) {
+			var word1 = 'post';
+			var word2 = 'was';
+		}
+		else {
+			var word1 = 'posts';
+			var word2 = 'were';
+		}
+		//put the alert on the page
+		$('body').append('<p id="hn-dn-alert">' + howMany + ' Medium ' + word1 + ' ' + word2 + ' hidden.</p>');
+		// $('#hn-dn-alert').css({
+		// 	'position': 'fixed',
+		// 	'top': '0',
+		// 	'right': '0',
+		// 	'padding': '15px 10px',
+		// 	'margin': '10px',
+		// 	'background-color': '#89C595',
+		// 	'font-family': 'helvetica, sans-serif',
+		// 	'font-weight': 'bold',
+		// 	'font-size': '14px',
+		// 	'border-radius': '10px',
+		// 	'border': '1px solid #217D34'
+		// });
+		setTimeout(function(){
+			$('#hn-dn-alert').fadeOut(500, function(){
+				$(this).remove();
+			});
+		}, 1000);
+	}
+}
+else {
 	//do nothing
 }
