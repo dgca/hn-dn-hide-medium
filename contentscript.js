fef3f00581f7ed1bffb7f12b0519cd0f3b00e320
Daniel Cortes <daniel.cortes@punchkickinteractive.com>
2014-07-03 17:33:33 -0500

    Made the alert a function, added font color to alert.

--- contentscript.js
+++ contentscript.js
@@ -1,11 +1,36 @@
 var host = window.location.host;
 
+function displayAlert(howMany) {
+	//say how many were removed
+	//singular or plural?!
+	if (howMany < 2) {
+		var word1 = 'post';
+		var word2 = 'was';
+	}
+	else {
+		var word1 = 'posts';
+		var word2 = 'were';
+	}
+	//put the alert on the page
+$('body').append('<p id="hn-dn-alert">' + howMany + ' Medium ' + word1 + ' ' + word2 + ' hidden.</p>');
+	setTimeout(function(){
+		$('#hn-dn-alert').fadeOut(500, function(){
+			$(this).remove();
+		});
+	}, 1000);
+}
+
 if (host === 'news.ycombinator.com') {
-	//remove author and comments data
-	$('.comhead:contains("medium.com")').parent().parent().next().next().css('display', 'none');
-	$('.comhead:contains("medium.com")').parent().parent().next().css('display', 'none');
-	//remove title row
-	$('.comhead:contains("medium.com")').parent().parent().css('display', 'none');
+	var howMany = $('.comhead:contains("medium.com")').length;
+
+	if (howMany > 0) {
+		//remove author and comments data
+		$('.comhead:contains("medium.com")').parent().parent().next().next().css('display', 'none');
+		$('.comhead:contains("medium.com")').parent().parent().next().css('display', 'none');
+		//remove title row
+		$('.comhead:contains("medium.com")').parent().parent().css('display', 'none');
+		displayAlert(howMany);
+	}
 }
 else if (host === 'news.layervault.com') {
 	//count removed items
@@ -14,24 +39,7 @@ else if (host === 'news.layervault.com') {
 	if (howMany > 0) {
 		//remove items
 		$('span:contains("medium.com")').parent().parent().css('display', 'none');
-
-		//say how many were removed
-		//singular or plural?!
-		if (howMany < 2) {
-			var word1 = 'post';
-			var word2 = 'was';
-		}
-		else {
-			var word1 = 'posts';
-			var word2 = 'were';
-		}
-		//put the alert on the page
-		$('body').append('<p id="hn-dn-alert">' + howMany + ' Medium ' + word1 + ' ' + word2 + ' hidden.</p>');
-		setTimeout(function(){
-			$('#hn-dn-alert').fadeOut(500, function(){
-				$(this).remove();
-			});
-		}, 1000);
+		displayAlert(howMany);
 	}
 }
 else {
