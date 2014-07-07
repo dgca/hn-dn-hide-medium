f21607794ec6a73c3d29ebeb950cdae8eeb09e60
Daniel Cortes <daniel.cortes@punchkickinteractive.com>
2014-07-07 13:22:24 -0500

    Stopped trying to add text badge. Moving onto on/off functionality.

--- background.js
+++ background.js
@@ -1,16 +1,1 @@
-//not working ATM
-
-var host = window.location.host;
-
-if (host === 'news.ycombinator.com') {
-	var howMany = $('.comhead:contains("medium.com")').length;
-	chrome.browserAction.setBadgeText({'text': howMany});
-
-}
-else if (host === 'news.layervault.com') {
-	var howMany = $('span:contains("medium.com")').length;
-	chrome.browserAction.setBadgeText({'text': howMany});
-}
-else {
-	//do nothing
-}
+// Eventually, I'll figure out how badges work.
