b23ee15d76dfc7ed159783dee7e0157a24ee5b8b
Daniel Cortes <daniel.cortes@punchkickinteractive.com>
2014-07-08 15:29:16 -0500

    Remember on/off status from previous session.

--- background.js
+++ background.js
@@ -1,25 +1,13 @@
-
-
-// chrome.storage.local.set({'isPaused': false});
-// chrome.browserAction.setIcon({path: "images/on.png"});
-
-chrome.storage.local.get('isPaused',
-	function(value) {
-		if (value['isPaused'] == undefined ) {
-			chrome.browserAction.setIcon({path: "images/on.png"});
-			chrome.storage.local.set({'isPaused': false});
-		}
-		else if (value['isPaused'] == false) {
-			chrome.browserAction.onClicked.addListener(function() {
-				chrome.browserAction.setIcon({path: "images/off.png"});
-				chrome.storage.local.set({'isPaused': true});
-			});
-		}
-		else {
-			chrome.browserAction.onClicked.addListener(function() {
-				chrome.browserAction.setIcon({path: "images/on.png"});
-				chrome.storage.local.set({'isPaused': false});
-			});
-		}
-	}
-);
+chrome.browserAction.onClicked.addListener(function() {
+  chrome.storage.local.get('isPaused',
+    function(value) {
+      if (value['isPaused'] == false) {
+        chrome.browserAction.setIcon({path: "images/off.png"});
+        chrome.storage.local.set({'isPaused': true});
+      } else {
+        chrome.browserAction.setIcon({path: "images/on.png"});
+        chrome.storage.local.set({'isPaused': false});
+      }
+    }
+  );
+});
