e129c7a69161af9d03e26a3972fb923fc617fe21
Daniel Cortes <daniel.cortes@punchkickinteractive.com>
2014-07-08 10:53:35 -0500

    Added on/off functionality.

--- background.js
+++ background.js
@@ -1,1 +1,17 @@
-// Eventually, I'll figure out how badges work.
+var paused = false;
+
+chrome.storage.local.set({'isPaused': false});
+
+chrome.browserAction.onClicked.addListener(function() {
+	paused = !paused
+	if (paused) {
+		chrome.browserAction.setIcon({path: "images/off.png"});
+		chrome.storage.local.set({'isPaused': true});
+		/* pause */
+	}
+	else {
+		chrome.browserAction.setIcon({path: "images/on.png"});
+		chrome.storage.local.set({'isPaused': false});
+		/* continue */
+	}
+});
--- contentscript.js
+++ contentscript.js
@@ -1,47 +1,70 @@
-var host = window.location.host;
+chrome.storage.local.get('isPaused',
+	function(value){
 
-function displayAlert(howMany) {
-	//say how many were removed
-	//singular or plural?!
-	if (howMany < 2) {
-		var word1 = 'post';
-		var word2 = 'was';
-	}
-	else {
-		var word1 = 'posts';
-		var word2 = 'were';
-	}
-	//put the alert on the page
-$('body').append('<p id="hn-dn-alert">' + howMany + ' Medium ' + word1 + ' ' + word2 + ' hidden.</p>');
-	setTimeout(function(){
-		$('#hn-dn-alert').fadeOut(500, function(){
-			$(this).remove();
-		});
-	}, 1000);
-}
+		/**
+		This is probably not the best way to do this.
+		But, it works.
+		Love,
+		Percy
+		**/
 
-if (host === 'news.ycombinator.com') {
-	var howMany = $('.comhead:contains("medium.com")').length;
+		// The closure we pass to the chrome.storage.local.get function is passed a parameter containing the value set in background.js using chrome.storage.local.set
+		// In background.js we set an array to Chrome local storage, where isPaused is the key and the value is the boolean variable (plugin on or off)
+		// Here, we grab that value by using the same key we set in background.js.
 
-	if (howMany > 0) {
-		//remove author and comments data
-		$('.comhead:contains("medium.com")').parent().parent().next().next().css('display', 'none');
-		$('.comhead:contains("medium.com")').parent().parent().next().css('display', 'none');
-		//remove title row
-		$('.comhead:contains("medium.com")').parent().parent().css('display', 'none');
-		displayAlert(howMany);
-	}
-}
-else if (host === 'news.layervault.com') {
-	//count removed items
-	var howMany = $('span:contains("medium.com")').length;
+		isPaused = value['isPaused'];
 
-	if (howMany > 0) {
-		//remove items
-		$('span:contains("medium.com")').parent().parent().css('display', 'none');
-		displayAlert(howMany);
+		if (isPaused == false) {
+
+			var host = window.location.host;
+
+			function displayAlert(howMany) {
+				//say how many were removed
+				//singular or plural?!
+				if (howMany < 2) {
+					var word1 = 'post';
+					var word2 = 'was';
+				}
+				else {
+					var word1 = 'posts';
+					var word2 = 'were';
+				}
+				//put the alert on the page
+			$('body').append('<p id="hn-dn-alert">' + howMany + ' Medium ' + word1 + ' ' + word2 + ' hidden.</p>');
+				setTimeout(function(){
+					$('#hn-dn-alert').fadeOut(500, function(){
+						$(this).remove();
+					});
+				}, 1000);
+			}
+
+			if (host === 'news.ycombinator.com') {
+				var howMany = $('.comhead:contains("medium.com")').length;
+
+				if (howMany > 0) {
+					//remove author and comments data
+					$('.comhead:contains("medium.com")').parent().parent().next().next().css('display', 'none');
+					$('.comhead:contains("medium.com")').parent().parent().next().css('display', 'none');
+					//remove title row
+					$('.comhead:contains("medium.com")').parent().parent().css('display', 'none');
+					displayAlert(howMany);
+				}
+			}
+			else if (host === 'news.layervault.com') {
+				//count removed items
+				var howMany = $('span:contains("medium.com")').length;
+
+				if (howMany > 0) {
+					//remove items
+					$('span:contains("medium.com")').parent().parent().css('display', 'none');
+					displayAlert(howMany);
+				}
+			}
+			else {
+				//do nothing
+			}
+
+		}
 	}
-}
-else {
-	//do nothing
-}
+
+);
