69f2a3503f2ea6fd89679cf01dfcfdea8be0c585
Daniel Cortes <daniel.cortes@punchkickinteractive.com>
2014-07-08 21:47:47 -0500

    Bug fixing the on/of functionality.

--- background.js
+++ background.js
@@ -1,13 +1,23 @@
+chrome.storage.local.get('isPaused',
+	function(value) {
+		if (value['isPaused'] == undefined) {
+			chrome.storage.local.set({'isPaused': false});
+			console.log(value);
+		}
+	});
+
 chrome.browserAction.onClicked.addListener(function() {
   chrome.storage.local.get('isPaused',
     function(value) {
       if (value['isPaused'] == false) {
         chrome.browserAction.setIcon({path: "images/off.png"});
         chrome.storage.local.set({'isPaused': true});
-      } else {
+      }
+			else {
         chrome.browserAction.setIcon({path: "images/on.png"});
         chrome.storage.local.set({'isPaused': false});
       }
+			console.log(value);
     }
   );
 });
